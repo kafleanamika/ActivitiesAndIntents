a050c34e6a6f0eea98330b565c93a537bceda83f
Anamika Kafle <kafleanamika123@gmail.com>
2021-03-18 22:13:54 +0545

    third commit

---
+++ app/src/main/java/me/mahakagg/helloToastHW1/MainActivity.java
@@ -0,0 +1,34 @@
+package me.mahakagg.helloToastHW1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_COUNT = "me.mahakagg.helloToastHW1.COUNT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+        mCount ++;
+        if (mShowCount != null){
+            mShowCount.setText(String.format(Locale.ENGLISH, "%d", mCount));
+        }
+    }
+
+    public void sayHello(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra(EXTRA_COUNT, mCount);
+        startActivity(intent);
+    }
+}
